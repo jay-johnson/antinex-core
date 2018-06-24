a2084d00eb49ca9484b7b32a24dcb7cf04a3545d
jay-johnson <jay.p.h.johnson@gmail.com>
2018-06-24 08:54:25 -0700

    search tool fix for base container

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "antinex_core"))
 setup(
     name="antinex-core",
     cmdclass={"test": PyTest},
-    version="1.0.60",
+    version="1.0.61",
     description=("AntiNex publisher-subscriber core for processing "
                  "training and prediction requests for deep neural "
                  "networks to detect network exploits using Keras "
